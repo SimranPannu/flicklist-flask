68ccee2c9a032186690fe099242e9e8224b41307
Simran Pannu <simpannu14@gmail.com>
2019-09-05 19:47:50 -0400

    Short, descriptive message here

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import random
 from flask import Flask
 
 app = Flask(__name__)
@@ -14,16 +15,26 @@ def index():
     content += "<ul>"
     content += "<li>" + movie + "</li>"
     content += "</ul>"
+   
+    
+    
 
+    tomorrow_movie = get_random_movie()
     # TODO: pick another random movie, and display it under
     # the heading "<h1>Tommorrow's Movie</h1>"
+    content += "<h1>Tommorrow's Movie</h1>"
+    content += "<ul>"
+    content += "<li>" + tomorrow_movie + "</li>"
+    content += "</ul>"
 
     return content
 
 def get_random_movie():
     # TODO: make a list with at least 5 movie titles
+    my_list=["The Big Lebowski","Fast and Furios","The Lion king","Aladdin","Toy Story"]
     # TODO: randomly choose one of the movies, and return it
-    return "The Big Lebowski"
+    movie=random.randint(0,4)
+    return my_list[movie]
 
 
 app.run()
